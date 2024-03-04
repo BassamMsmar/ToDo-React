ddc755498735b41bb9ff2fd3d994b764300c1723
Bassam Msmar <bassammsmar@gmail.com>
2024-03-04 14:26:01 +0300

    add setEffect and setState

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+function About() {
+    return (
+
+        <><h1>hi bassam</h1>
+        <Link href="/">Home</Link></>
+
+    )
+}
+export default About    
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,54 @@
+'use client';
+
 import Image from "next/image";
 import styles from "./page.module.css";
 import ProductList from "@/componints/productList";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 
 
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.center}>
 
-      <ProductList text="hello" number={123} />
-      
+
+  let hideProductPist = true
+  let text = "hello"
+  let number = 123    
+
+
+  const [counter, setCounter] = useState(0)
+  const [success, setSuccess] = useState(false)
+  console.log(success)
+
+  const handleClick = () =>{
+    setCounter(counter + 1)
+    if (counter === 10){
+      setSuccess(true)
+    }
+  }
+
+  useEffect(() => {
+    console.log("Event is true")
+  }, [success])
+
+  useEffect(() => {
+    console.log("counter is " + counter)
+  }, [counter])
+
+
+  return (
+    <main className={styles.main} >
+      <div>
+        <h1>Conter : {counter}</h1>
+        <button type="button" onClick={handleClick}>Click</button>
       </div>
+      
+      <div className={styles.center}>
+      {hideProductPist && <ProductList text={text} number={number} />}
+      </div>
+      <a href="/about">About</a>
+      <Link href="/about">About</Link>
     </main>
   );
 }
